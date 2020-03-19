bec1d36727c496a7f1095cfd8a9083da37bce791
Ohad Shulz <O.Shulz@f5.com>
2020-03-19 13:50:18 +0000

    inital commit

---
+++ FireLight/FireLight.swift
@@ -0,0 +1,15 @@
+//
+//  FireLight.swift
+//  FireLight
+//
+//  Created by Ohad Shulz on 19/03/2020.
+//  Copyright © 2020 firelight. All rights reserved.
+//
+
+import UIKit
+
+class FireLight: NSObject {
+    func say(something: String) {
+        NSLog(something)
+    }
+}
